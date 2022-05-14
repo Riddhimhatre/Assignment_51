a6d7057249d36149b8323adae0e524d0f5851634
Riddhimhatre <99936013+Riddhimhatre@users.noreply.github.com>
2022-05-14 09:18:25 +0530

    Add files via upload

--- students.js
+++ students.js
@@ -1,153 +1,17 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const { getJsonData } = require('./data-provider');
-const data = require('./json-data.json');
-// const Student = require('../models/studentdata.js');
+const express = require("express");
 
+const  student_Act = require("../controllers/students"); 
 
 const router = express.Router();
 
-const getStudents = async (req, res) => {
-    try {
-        const students = await data;
+router.get('/', student_Act.getStudents); /* Getting al the studs */
+router.get('/:uid', student_Act.getspecStudent); /* Getting a particular stud by id */
+router.get('/:uid/subjects', student_Act.getStuSubjects) /* Getting all the subjects of that stud */
+router.get('/:uid/subjects/:index/marks', student_Act.getStuSubjectMarks) /*  */
 
-        res.render('students-list',
-            {
-                students,
-                pageTitle: 'Students',
-                path: '/students',
-                stud: null,
-                index: 0
-            })
-        return
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
-}
+router.post('/', student_Act.createstudent);
 
-const getspecStudent = async (req, res) => {
-    const uid = req.params.uid;
+router.patch('/:uid', student_Act.updatestudent);
+router.delete('/:uid', student_Act.deletestudent);
 
-    try {
-        // const stud = await Student.findOne({ uid: uid });
-        const stud = data.filter(stu => +stu.uid == uid)[0];
-        console.log({ stud: stud });
-        res.render('student-details',
-            {
-                stud: stud,
-                pageTitle: stud.name.toUpperCase(),
-                path: `/students/${stud.uid}`,
-                index: 0
-            })
-        return
-        res.status(200).json(stud);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
-}
-
-/* For getting student subjects */
-const getStuSubjects = async (req, res) => {
-    const uid = req.params.uid;
-
-    // const student = await Student.find();
-    const student = data.filter(stu => +stu.uid == uid)[0];
-    res.render('student-subjects',
-        {
-            subjects: student.subjects,
-            student,
-            pageTitle: `Subjects - ${student.name.toUpperCase()}`,
-            path: `/students/${student.uid}/subjects`,
-            stud: student,
-            index: 0
-        })
-    return
-}
-
-/* get students subject marks */
-const getStuSubjectMarks = async (req, res) => {
-    const {
-        uid,
-        index
-    } = req.params
-
-    const student = data.filter(stu => +stu.uid == uid)[0];
-    res.render('marks',
-        {
-            subject: student.subjects[index],
-            marks: student.marks[index],
-            student,
-            stud: student,
-            pageTitle: `Subjects - Marks - ${student.name.toUpperCase()}`,
-            path: `/students/${student.uid}/subjects/${index}/marks`,
-            index
-        })
-    return
-}
-
-/* For creating a student */
-const createstudent = async (req, res) => {
-    console.log(req.body);
-    exist = await Student.exists({ uid: req.body.uid });
-    console.log({
-        exist
-    });
-    if (exist) {
-        return res.status(400).json({ message: "Student already exists" });
-    }
-
-    const newstudent = new Student({
-        name: req.body.name,
-        uid: req.body.uid,
-        semester: req.body.semester,
-        subjects: req.body.subjects.split(','),
-        term: req.body.term,
-        marks: req.body.marks.split(',')
-    })
-    try {
-        await newstudent.save();
-
-        res.status(201).json(newstudent);
-
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
-
-}
-
-const updatestudent = async (req, res) => {
-    const uid = req.params.uid;
-    try {
-        await Student.findOneAndUpdate({
-            uid: uid,
-        },
-            req.body
-        )
-        res.status(202).json({ uid: uid });
-
-    } catch (error) {
-        res.status(401).json({ message: error.message });
-    }
-
-}
-
-const deletestudent = async (req, res) => {
-    const uid = req.params.uid;
-
-    try {
-        await Student.findOneAndRemove({ uid: uid });
-        res.status(203).json({ uid: uid });
-
-    } catch (error) {
-        res.status(402).json({ message: error.message });
-    }
-}
-
-module.exports.getStudents = getStudents;
-module.exports.createstudent = createstudent;
-module.exports.getspecStudent = getspecStudent;
-module.exports.updatestudent = updatestudent;
-module.exports.deletestudent = deletestudent;
-
-module.exports.getStuSubjects = getStuSubjects;
-module.exports.getStuSubjectMarks = getStuSubjectMarks;
+module.exports=router;
